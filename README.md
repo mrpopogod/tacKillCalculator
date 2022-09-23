726da8f33e18cce03b0f98b3e17f2aa8353312af
Aaron Doukas <amyleeslave@gmail.com>
2022-09-22 22:02:58 -0700

    Heatsinks and actuators, plus more notes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # tacKillCalculator
 
-Basic CLI utility to take an SSW mech file and calculate how likely it is to die when a TAC is rolled for both basic rules (TACs hit the CT only) and floating crits (location of TAC is rerolled).
+Basic CLI utility to take an SSW mech file and calculate how likely it is to die when a TAC is rolled for both basic rules (TACs hit the CT only) and floating crits (location of TAC is rerolled).  Does not support Tripods/Superheavies (if file parses no guarantees results are correct)
 
 Note: At this point in time the xml parsing library requires an xmlns attribute to exist on the root \<mech\> element; simply add xmlns="" and it can parse the SSW file.
--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ enum Crit {
     Other,
 }
 
+#[derive(Clone, Copy, PartialEq)]
 enum Location {
     Head,
     CenterTorso,
@@ -88,7 +89,7 @@ fn main() {
             "cockpit" => handle_cockpit(child, &mut mech),
             "gyro" => mech.gyro = child.text(),
             "baseloadout" => handle_loadout(child, &mut mech),
-            // TODO: "loadout"
+            // TODO: "loadout" - probably want to take as an option and match the right one
             _ => (),
         }
     }
@@ -97,6 +98,7 @@ fn main() {
     handle_engine_type(&mut mech);
     handle_gyro_type(&mut mech);
     populate_leg_crits(&mut mech);
+    // TODO: for full correctness should check for crits not exceeding the legal amount in each limb.
 
     // TODO: hardened armor modification to crit result
 
@@ -106,15 +108,15 @@ fn main() {
             2..=7 => (),
             8..=9 => {
                 let location_crits: Vec<Crit> = mech.ct_crits.to_vec();
-                check_single_crit(location_crits, &mut deaths);
+                check_single_crit(mech.clan_case, location_crits, &mut deaths);
             }
             10..=11 => {
                 let location_crits: Vec<Crit> = mech.ct_crits.to_vec();
-                check_double_crit(location_crits, &mut deaths);
+                check_double_crit(mech.clan_case, location_crits, &mut deaths);
             }
             12 => {
                 let location_crits: Vec<Crit> = mech.ct_crits.to_vec();
-                check_triple_crit(location_crits, &mut deaths);
+                check_triple_crit(mech.clan_case, location_crits, &mut deaths);
             }
             _ => panic!("2d6 somehow was not between 2 and 12"),
         }
@@ -137,13 +139,13 @@ fn main() {
         match two_d_six() {
             2..=7 => (),
             8..=9 => {
-                check_single_crit(location_crits, &mut floating_deaths);
+                check_single_crit(mech.clan_case, location_crits, &mut floating_deaths);
             }
             10..=11 => {
-                check_double_crit(location_crits, &mut floating_deaths);
+                check_double_crit(mech.clan_case, location_crits, &mut floating_deaths);
             }
             12 => {
-                check_triple_crit(location_crits, &mut floating_deaths);
+                check_triple_crit(mech.clan_case, location_crits, &mut floating_deaths);
             }
             _ => panic!("2d6 somehow was not between 2 and 12"),
         }
@@ -162,21 +164,21 @@ fn main() {
     );
 }
 
-fn check_single_crit(location_crits: Vec<Crit>, deaths: &mut i32) {
-    if location_crits.len() < 1 {
+fn check_single_crit(clan_case: bool, location_crits: Vec<Crit>, deaths: &mut i32) {
+    if location_crits.is_empty() {
         return;
     }
 
     let mut rng = rand::thread_rng();
     let chosen_crit = location_crits.choose(&mut rng).unwrap();
-    if *chosen_crit == Crit::Ammo || *chosen_crit == Crit::Cockpit {
+    if (*chosen_crit == Crit::Ammo && !clan_case) || *chosen_crit == Crit::Cockpit {
         *deaths += 1;
     }
 }
 
-fn check_double_crit(mut location_crits: Vec<Crit>, deaths: &mut i32) {
+fn check_double_crit(clan_case: bool, mut location_crits: Vec<Crit>, deaths: &mut i32) {
     if location_crits.len() < 2 {
-        check_single_crit(location_crits, deaths);
+        check_single_crit(clan_case, location_crits, deaths);
         return;
     }
 
@@ -196,8 +198,7 @@ fn check_double_crit(mut location_crits: Vec<Crit>, deaths: &mut i32) {
     let second_crit = *second_crit;
     location_crits.remove(i);
 
-    if first_crit == Crit::Ammo
-        || second_crit == Crit::Ammo
+    if ((first_crit == Crit::Ammo || second_crit == Crit::Ammo) && !clan_case)
         || first_crit == Crit::Cockpit
         || second_crit == Crit::Cockpit
     {
@@ -207,9 +208,9 @@ fn check_double_crit(mut location_crits: Vec<Crit>, deaths: &mut i32) {
     }
 }
 
-fn check_triple_crit(mut location_crits: Vec<Crit>, deaths: &mut i32) {
+fn check_triple_crit(clan_case: bool, mut location_crits: Vec<Crit>, deaths: &mut i32) {
     if location_crits.len() < 3 {
-        check_double_crit(location_crits, deaths);
+        check_double_crit(clan_case, location_crits, deaths);
         return;
     }
 
@@ -236,9 +237,14 @@ fn check_triple_crit(mut location_crits: Vec<Crit>, deaths: &mut i32) {
     let third_crit = *third_crit;
     location_crits.remove(i);
 
-    if first_crit == Crit::Ammo
-        || second_crit == Crit::Ammo
-        || third_crit == Crit::Ammo
+    // TODO: refactor
+    // 1. after we remove a crit, check for head, kill if appropriate
+    // 2. from that same crit, check for ammo, if ammo check for case, no case then kill
+    // 3. if regular case check for gyro, cockpit, or enough engine crits for death
+    // 4. if case ii roll another crit
+    // 5. when remove engine/gyro add to a running tally and check at end (and reference as needed in step 3)
+    if ((first_crit == Crit::Ammo || second_crit == Crit::Ammo || third_crit == Crit::Ammo)
+        && !clan_case)
         || first_crit == Crit::Cockpit
         || second_crit == Crit::Cockpit
         || third_crit == Crit::Cockpit
@@ -258,6 +264,8 @@ fn check_triple_crit(mut location_crits: Vec<Crit>, deaths: &mut i32) {
 }
 
 fn populate_leg_crits(mech: &mut Mech) {
+    // TODO: handle quads
+    // hip, upper, lower, foot
     mech.ll_crits.push(Crit::Other);
     mech.ll_crits.push(Crit::Other);
     mech.ll_crits.push(Crit::Other);
@@ -391,10 +399,77 @@ fn handle_loadout(child: &Element, mech: &mut Mech) {
         match loadout_child.name().to_string().as_str() {
             "clancase" => mech.clan_case = loadout_child.text() == "TRUE",
             "equipment" => handle_equipment(loadout_child, mech),
-            "actuators" => (), // attributes, lla, lh, rla, rh
-            "heatsinks" => (), // type element, location element per HS
+            "actuators" => handle_actuators(loadout_child, mech),
+            "heatsinks" => {
+                let mut hs_type = String::from("");
+                let mut hs_locations: Vec<Location> = Vec::new();
+                for heatsink_child in loadout_child.children() {
+                    if heatsink_child.name() == "type" {
+                        hs_type = heatsink_child.text();
+                    } else if heatsink_child.name() == "location" {
+                        match heatsink_child.text().as_str() {
+                            "RT" => hs_locations.push(Location::RightTorso),
+                            "LT" => hs_locations.push(Location::LeftTorso),
+                            "RA" => hs_locations.push(Location::RightArm),
+                            "LA" => hs_locations.push(Location::LeftArm),
+                            "RL" => hs_locations.push(Location::RightLeg),
+                            "LL" => hs_locations.push(Location::LeftLeg),
+                            "CT" => hs_locations.push(Location::CenterTorso),
+                            "HD" => hs_locations.push(Location::Head),
+                            _ => panic!("Invalid heatsink location {}", heatsink_child.text()),
+                        }
+                    }
+                }
+
+                // TODO: IS vs Clan DHS
+                for hs_location in hs_locations {
+                    match hs_type.as_str() {
+                        "Single Heat Sink" => add_crit_to_location(Crit::Other, hs_location, mech),
+                        "Double Heat Sink" => {
+                            add_crit_to_location(Crit::Other, hs_location, mech);
+                            add_crit_to_location(Crit::Other, hs_location, mech);
+                            add_crit_to_location(Crit::Other, hs_location, mech);
+                        }
+                        _ => panic!("Invalid heat sink type {}", hs_type),
+                    }
+                }
+            }
             _ => (),
         }
+    }
+}
+
+fn add_crit_to_location(crit: Crit, location: Location, mech: &mut Mech) {
+    match location {
+        Location::Head => mech.head_crits.push(crit),
+        Location::CenterTorso => mech.ct_crits.push(crit),
+        Location::LeftTorso => mech.lt_crits.push(crit),
+        Location::RightTorso => mech.rt_crits.push(crit),
+        Location::LeftArm => mech.la_crits.push(crit),
+        Location::RightArm => mech.ra_crits.push(crit),
+        Location::LeftLeg => mech.ll_crits.push(crit),
+        Location::RightLeg => mech.rl_crits.push(crit),
+    }
+}
+
+fn handle_actuators(loadout_child: &Element, mech: &mut Mech) {
+    // Shoulder and upper arm
+    mech.la_crits.push(Crit::Other);
+    mech.la_crits.push(Crit::Other);
+    mech.ra_crits.push(Crit::Other);
+    mech.ra_crits.push(Crit::Other);
+
+    if let Some("TRUE") = loadout_child.attr("lla") {
+        mech.la_crits.push(Crit::Other);
+    }
+    if let Some("TRUE") = loadout_child.attr("lh") {
+        mech.la_crits.push(Crit::Other);
+    }
+    if let Some("TRUE") = loadout_child.attr("rla") {
+        mech.ra_crits.push(Crit::Other);
+    }
+    if let Some("TRUE") = loadout_child.attr("rh") {
+        mech.ra_crits.push(Crit::Other);
     }
 }
 
@@ -404,16 +479,9 @@ fn handle_equipment(loadout_child: &Element, mech: &mut Mech) {
     for equipment_child in loadout_child.children() {
         extract_equipment_type_and_location(equipment_child, &mut location, &mut equipment_type);
     }
-    match location {
-        Location::Head => mech.head_crits.push(equipment_type),
-        Location::CenterTorso => mech.ct_crits.push(equipment_type),
-        Location::LeftTorso => mech.lt_crits.push(equipment_type),
-        Location::RightTorso => mech.rt_crits.push(equipment_type),
-        Location::LeftArm => mech.la_crits.push(equipment_type),
-        Location::RightArm => mech.ra_crits.push(equipment_type),
-        Location::LeftLeg => mech.ll_crits.push(equipment_type),
-        Location::RightLeg => mech.rl_crits.push(equipment_type),
-    }
+
+    // TODO: biggest miss here is that we don't know the number of crits per piece of gear since that is hardcoded in SSW's code
+    add_crit_to_location(equipment_type, location, mech);
 }
 
 fn extract_equipment_type_and_location(
@@ -431,14 +499,14 @@ fn extract_equipment_type_and_location(
             "LL" => *location = Location::LeftLeg,
             "CT" => *location = Location::CenterTorso,
             "HD" => *location = Location::Head,
-            _ => panic!("Invalid equipment location"),
+            _ => panic!("Invalid equipment location {}", equipment_child.text()),
         }
     } else if equipment_child.name() == "type" {
         match equipment_child.text().as_str() {
             "ammunition" => *equipment_type = Crit::Ammo,
             _ => *equipment_type = Crit::Other,
         }
-    } // TODO: probably need to handle non-crittable equipment
+    } // TODO: any non-armor/structure equipment that ignores crits?
 }
 
 pub fn two_d_six() -> u8 {
